f3efdaad1bfe575c40e9078dc13b7d4b2c8ea8b1
jakebareng <barengjake@gmail.com>
2025-11-16 20:20:49 -0800

    Refactor SocketWrapper class to add type hints for improved clarity and type safety

--- Transport/socket_wrapper.py
+++ Transport/socket_wrapper.py
@@ -1,7 +1,12 @@
 import socket
+from typing import Tuple, Optional
+
 
 class SocketWrapper:
-    def __init__(self, local_addr=None, remote_addr=None):
+    def __init__(self, 
+                 local_addr: Optional[Tuple[str, int]] = None, 
+                 remote_addr: Optional[Tuple[str, int]] = None):
+
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.local_addr = local_addr
         self.remote_addr = remote_addr
@@ -9,13 +14,13 @@ class SocketWrapper:
             self.sock.bind(local_addr)
 
 
-    def bind(self, addr):
+    def bind(self, addr: Tuple[str, int]):
         self.sock.bind(addr)
 
-    def send_segment(self, segment):
+    def send_segment(self, segment: bytes):
         self.sock.sendto(segment, self.remote_addr)
 
-    def recv_segment(self, timeout=None):
+    def recv_segment(self, timeout: Optional[float] = None):
         if timeout is not None:
             self.sock.settimeout(timeout)
         return self.sock.recvfrom(1024)
