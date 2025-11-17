64e85f0e46a6ff169749fd852870f2ecfd382e36
jakebareng <barengjake@gmail.com>
2025-11-16 20:13:45 -0800

    Add initial implementation of Protocol and Transport modules with SocketWrapper class

---
+++ Transport/socket_wrapper.py
@@ -0,0 +1,24 @@
+import socket
+
+class SocketWrapper:
+    def __init__(self, local_addr=None, remote_addr=None):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.local_addr = local_addr
+        self.remote_addr = remote_addr
+        if local_addr is not None:
+            self.sock.bind(local_addr)
+
+
+    def bind(self, addr):
+        self.sock.bind(addr)
+
+    def send_segment(self, segment):
+        self.sock.sendto(segment, self.remote_addr)
+
+    def recv_segment(self, timeout=None):
+        if timeout is not None:
+            self.sock.settimeout(timeout)
+        return self.sock.recvfrom(1024)
+
+    def close(self):
+        self.sock.close()
